d3f2b5de0e2ce26cc9456612d231eb5b47a3667d
Andrew Pomerleau <arpomerleau@gmail.com>
2022-07-21 18:18:06 -0400

    refactored calculations out to new Circle class. Completed bonus missions for data entry validation.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,28 +2,35 @@ package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
 
+
 public class Area {
     private static final Scanner console = new Scanner(System.in);
-    private static final double pi = 3.14;
 
     public static void calculate() {
         boolean validInput = false;
         double radius = 0;
 
         while (!validInput) {
-            System.out.print("Enter a raduis: ");
+            System.out.print("Enter a radius: ");
             String input = console.nextLine();
 
             try {
                 radius = Double.parseDouble(input);
+
+                if (radius <= 0) {
+                    System.out.println("Error: Please enter a positive decimal figure.");
+                    continue;
+                }
+
                 validInput = true;
             } catch (NumberFormatException e) {
-                System.out.println("Error: Please enter a decimal number and try again.");
+                System.out.println("Error: Not a valid numerical entry. Exiting");
+                System.exit(0);
             }
         }
 
-        double area = pi * radius * radius;
-        System.out.println("The area of a circle radius " + radius + " is: " + area);
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
     }
 
     public static void main(String[] args) {
