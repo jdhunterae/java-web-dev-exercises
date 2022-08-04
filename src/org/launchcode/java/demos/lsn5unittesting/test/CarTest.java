5a75ddcd3129eebfc7dd59d5a80f74012b11d70d
Andrew Pomerleaum <arpomerleau@gmail.com>
2022-08-04 14:44:05 -0400

    new Test for after driving the car to check gasLevels

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -24,7 +24,12 @@ public class CarTest {
         assertEquals(10, test_car.getGasTankLevel(), .001);
     }
 
-    //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
 
